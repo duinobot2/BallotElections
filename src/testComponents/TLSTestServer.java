63378a89a5adb2c69f4ffbeecf3e038da684af17
duinobot2 <duinobot2@gmail.com>
2022-08-28 09:51:34 +0200

    test change absolute path with relative path

--- src/testComponents/TLSTestServer.java
+++ src/testComponents/TLSTestServer.java
@@ -20,9 +20,9 @@ public class TLSTestServer {
      */
     public static void main(String[] args) throws IOException{
         // TODO code application logic here
-        System.setProperty("javax.net.ssl.keyStore", "D:\\duino\\Google Drive (antonello.avella@iisfocaccia.edu.it)\\2022\\AlgeProtSicurezza\\ProjectElections\\BallotElections\\src\\ballotelections\\keystoreServer.jks");
+        System.setProperty("javax.net.ssl.keyStore", ".\\src\\testComponents\\keystoreServer.jks");
         System.setProperty("javax.net.ssl.keyStorePassword", "changeit");
-        System.setProperty("javax.net.ssl.trustStore", "D:\\duino\\Google Drive (antonello.avella@iisfocaccia.edu.it)\\2022\\AlgeProtSicurezza\\ProjectElections\\BallotElections\\src\\ballotelections\\keystoreClient.jks");
+        System.setProperty("javax.net.ssl.trustStore", ".\\src\\testComponents\\keystoreClient.jks");
         System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
         TLSServerBidi server = new TLSServerBidi(4000);
         
